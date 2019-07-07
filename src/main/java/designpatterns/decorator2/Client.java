1be748b03a8f99b70a6dac4b8b88a111a38dec77
dustin k <none@example.com>
2019-07-06 19:11:52 -0500

    add design patterns

---
+++ src/main/java/designpatterns/decorator2/Client.java
@@ -0,0 +1,26 @@
+package designpatterns.decorator2;
+
+import java.io.*;
+
+public class Client {
+    public static void main(String[] args)
+    {
+        int c;
+
+        StringBuffer stringBuffer = new StringBuffer("Jason Fedin aaaBBBBcccDDDD");
+        byte[] bytes = stringBuffer.toString().getBytes();
+
+        try {
+            InputStream in = new LowerCaseInputStream(new ByteArrayInputStream(bytes));
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
