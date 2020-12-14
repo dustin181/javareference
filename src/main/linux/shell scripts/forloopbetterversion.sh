1e58dfd2ad815422ed3bf60daf248a660cf315ae
Your Name <you@example.com>
2020-12-14 13:51:05 -0600

    add shell script examples

---
+++ src/main/linux/shell scripts/forloopbetterversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+COLORS="red green blue yellow"
+for COLOR in $COLORS
+do
+   echo "COLOR: $COLOR"
+done
---
+++ src/main/linux/shell scripts/useargumentsvariableamount.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Executing script: $0"
+
+for USER IN $@
+do
+   echo "Archiving user: $USER"
+   # Lock the account
+   passwd -l $USER
+
+   # Create an archive of the home directory.
+   tar cf /archives/${USER}.tar.gz /home/${USER}
+done
