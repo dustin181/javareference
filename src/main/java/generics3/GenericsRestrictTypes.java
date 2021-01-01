1b28f008e5afc4479e18c5029ec8245ca03781ad
Your Name <you@example.com>
2021-01-01 03:19:25 -0600

    add interface notes

--- src/main/java/generics3/GenericsRestrictTypes.java
+++ src/main/java/generics3/GenericsRestrictTypes.java
@@ -5,7 +5,16 @@ import java.util.List;
 
 public class GenericsRestrictTypes {
 
+
+    //Two different choices for the same thing, but this is simpler
     public static double sum(List<? extends Number> numberlist) {
+        double sum = 0.0;
+        for (Number n : numberlist) sum += n.doubleValue();
+        return sum;
+    }
+
+    //Two different choices for the same thing, use when you need to refer to it in the method
+    public static <T extends Number> double sum2(List<T> numberlist) {
         double sum = 0.0;
         for (Number n : numberlist) sum += n.doubleValue();
         return sum;
